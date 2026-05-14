ae5caf3f09b2c25b198d4d5253714e5064a4520e
Han <rayhan.dita45@smk.belajar.id>
2026-05-15 01:25:18 +0700

    fix: include main.go in cmd/gonode and fix gitignore

---
+++ cmd/gonode/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"gonode/pkg/engine"
+	"gonode/pkg/utils"
+)
+
+func main() {
+	// Auto-initialize installation if binary is missing
+	utils.InitInstallation()
+
+	if len(os.Args) < 2 {
+		utils.PrintHelp()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "start":
+		engine.HandleStartCommand()
+	case "list":
+		engine.SendCommand("list")
+	case "stop":
+		engine.SendCommand("stop")
+	case "help":
+		utils.PrintHelp()
+	case "daemon-logic":
+		engine.RunDaemonLogic()
+	default:
+		utils.PrintHelp()
+	}
+}
